23e04186d46dc5ffddf486921bfc1fecf26568bd
Inna T <meeoow005@gmail.com>
2023-05-31 20:33:02 +0200

    fix: configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
-import { defineConfig } from 'vitest/config';
-
-// https://vitejs.dev/config/
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, 'src/components')
+    },
+  },
 });
