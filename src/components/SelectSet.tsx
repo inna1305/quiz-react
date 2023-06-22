b791d5c5ae60a78cbae1e5244acf5033a32c1fcf
Inna T <meeoow005@gmail.com>
2023-06-22 13:10:29 +0200

    fix: select view

--- src/components/SelectSet.tsx
+++ src/components/SelectSet.tsx
@@ -4,17 +4,13 @@ import { SetProps } from "types/types";
 const SelectSet = (props: SetProps): ReactElement => {
   const variants = Array.from(props.question.variants.values());
   const arrVariantsElem = variants.map((variant, index) => (
-        <option className="fieldset__option" id={variant} value={variant} key={index} onInput={() => {
-
-        }}></option>
+        <option className="fieldset__option" id={variant} value={variant} key={index}>{variant}</option>
   ));
 
   return (
-    <>
         <select multiple className="select">
           {arrVariantsElem}
         </select>
-    </>
   );
 };
 
